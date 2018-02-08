088164d3a6d02bd96c847a36c2a8cd80c7296b7f
MatthewL019 <projectliferp01@gmail.com>
2018-02-08 23:06:46 +0000

    made roles toggable and added 2 new commands
    Added !projects which shows currently available projects people can
    contribute to
    
    Added !stream, !twitch, !live to redirect to LILDINKED's twitch channel
    
    Made role switching only allow the user to be in one role, either
    frontend, backend, or fullstack. Before it would stack them all so a
    user could have all 3

--- index.js
+++ index.js
@@ -14,16 +14,20 @@ bot.on('ready', () => {
 bot.on('message', message => {
     // So the bot doesn't reply to itself
     if (message.author.bot) return;
-    
+
     // Check if the message starts with the prefix
     if (message.content.indexOf(config.prefix) === 0) {
         // Get the user's message excluding the prefix, and lower case it to avoid CaSe SeNsItIvE
         var text = message.content.substring(1).toLowerCase();
-        var response = ''; // ready a string for setting the response in
 
+        var response = ''; // init response
         // get the Member who sent the message.
         let member = message.member;
 
+        // Roles
+        const frontend_role = message.guild.roles.find("name", "frontend");
+        const backend_role = message.guild.roles.find("name", "backend");
+        const fullstack_role = message.guild.roles.find("name", "fullstack");
 
         // Switching through all strings after the prefix
         // e.g ![role frontend]    whatever is in the [] is the string we're switching through.
@@ -33,9 +37,23 @@ bot.on('message', message => {
             case "role backend":
             case "role fullstack":
                 let role_name = text.substring(5); // !role [frontend/backend/fullstack] - this gets the role name
-                let response = "You are now set as a " + role_name + " developer, " + message.author; // write a response
+                response = "You are now set as a " + role_name + " developer, " + message.author; // write a response
                 let role = message.guild.roles.find("name", role_name); // find the proper Role matching the role_name
+                member.removeRoles([frontend_role, backend_role, fullstack_role]);
                 member.addRole(role); // add the member to the role.
+                break;
+
+            // Display all current projects by the LLC community
+            case "projects":
+                response = "```\nCurrent projects\n================\n1. Discord Bot\nhttps://github.com/MattL019/discord-bot```";
+                break;
+
+
+            // Commands to redirect to LILDINKED's live stream.
+            case "stream":
+            case "twitch":
+            case "live":
+                response = "You can find LILDINKED's Twitch here: https://www.twitch.tv/lildinked";
                 break;
         }
 
@@ -43,6 +61,6 @@ bot.on('message', message => {
         if(response !== '') message.channel.send(response);
     }
 });
-  
+
 // log the bot in. (never remove this)
 bot.login(config.token);
